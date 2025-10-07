05a7d52a6f5fd946dcb12efffd7a1b6553e637e5
Julien Chaumont <julienc91@users.noreply.github.com>
2025-10-07 16:59:55 +0200

    Update same_file.py

--- same_file.py
+++ same_file.py
@@ -1,3 +1,9 @@
 # test_keyword_eric_3243
 
-SECRET_API_KEY = "X_mYjSu2STDj5g6k1vkG8MW4tdufIt4km9bYdzce2b5Bsg1cwO"
+SECRET_API_KEY = "X_mYjSu2STDj5g6k1vkG8MW4tdufIt4km9bYdzce2b5Bsg1cw1"  # julienc.dev
+
+"""
+Leaking for julienc.io
+
+And some secret grasper: test-secret-grasper-julien should work
+"""
