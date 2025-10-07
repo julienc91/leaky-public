b4bf628a8503a3c7d1da0158d8f4293f7f7e87eb
Julien Chaumont <julienc91@users.noreply.github.com>
2025-10-07 17:15:32 +0200

    Update same_file.py

--- same_file.py
+++ same_file.py
@@ -2,7 +2,7 @@
 
 SECRET_API_KEY = "X_mYjSu2STDj5g6k1vkG8MW4tdufIt4km9bYdzce2b5Bsg1cw1"  # julienc.dev
 """
-Leaking for julienc.io in prod
+Leaking for julienc.io in prod-julienc.dev
 
 And some secret grasper: test-secret-grasper-julien should work
 """
