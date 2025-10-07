6c9ff9330a42e08a55137dc72c3550ce9277c4bc
Julien Chaumont <julienc91@users.noreply.github.com>
2025-10-07 17:06:47 +0200

    forgot the prod tag

--- same_file.py
+++ same_file.py
@@ -1,9 +1,8 @@
 # test_keyword_eric_3243
 
 SECRET_API_KEY = "X_mYjSu2STDj5g6k1vkG8MW4tdufIt4km9bYdzce2b5Bsg1cw1"  # julienc.dev
-
 """
-Leaking for julienc.io
+Leaking for julienc.io in prod
 
 And some secret grasper: test-secret-grasper-julien should work
 """
