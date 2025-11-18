c4980381e91eada9e39be0c548c609bd359e323f
Julien Chaumont <julienc91@users.noreply.github.com>
2025-11-18 12:47:35 +0100

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,1 +1,7 @@
+# This may be a prod secret
 SECRET_API_KEY = "xSCiCpyptnDXPc2IWEryoczB3Q4FA3q03ZO1XQAUerUXwSX8WY"
+# JULIENC.DEV
+
+# Or maybe not a prod secret, who knows?
+# secrets@julienc.dev
+# @julienc.dev!
