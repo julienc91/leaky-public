d1f54e0acdfa81d3b169d7ec25ec0bfc8065507e
Julien Chaumont <julienc91@users.noreply.github.com>
2025-11-19 10:26:39 +0100

    Create prod.py

---
+++ prod.py
@@ -0,0 +1,4 @@
+# Possibly a prod secret from gitguardian
+SECRET_API_KEY = "yY28_cmFozfANUg-NXqRJIpx1pjb1KV4cDk0GG36WDRBmgG5Cy"
+# Or maybe not, who knows? Try contacting test@gitguardian.com
+# And ask them if this is a real prod secret
